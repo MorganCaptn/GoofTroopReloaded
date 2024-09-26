92ed0d725f7d521a9b0d05eafcb8c2193143fce3
NKMorgan <morganpoonk@googlemail.com>
2024-09-26 21:34:43 +0200

    Improved block switching mechanism

--- Assets/Scripts/BlockTrigger.cs
+++ Assets/Scripts/BlockTrigger.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class BlockTrigger : MonoBehaviour
 {
-    public int nextBlockID; // Die ID des Blocks, zu dem gewechselt wird
+    public GameObject spawnBlock;
+    private int nextBlockID; // Die ID des Blocks, zu dem gewechselt wird
     public GameManager gameManager; // Referenz zum GameManager
-    public Direction exitDirection; // In welche Richtung zeigt der Ausgang? Wichtig für den nachfolgenden Spawn der Player
+    void Start()
+    {
+        // Finde die nextBlockID durch Zugriff auf das Parent-Objekt von spawnBlock
+        if (spawnBlock.transform.parent != null)
+        {
+            // Prüfe, ob das Parent-Objekt ein Skript hat, das die Block-ID enthält
+            BlockManager nextBlockManager = spawnBlock.transform.parent.GetComponent<BlockManager>();
+
+            if (nextBlockManager != null)
+            {
+                nextBlockID = nextBlockManager.blockID; // Hier nehmen wir an, dass der Block eine blockID hat
+            }
+            else
+            {
+                Debug.LogError("Das Parent-Objekt hat keinen BlockManager.");
+            }
+        }
+        else
+        {
+            Debug.LogError("spawnBlock hat kein Parent-Objekt.");
+        }
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("BlockTrigger passiert.");
         // Überprüfen, ob der Spieler den Trigger betritt
         if (other.CompareTag("Player"))
         {
-        
-            var nextSpawnPos = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
-            //Find the correct spawn position for the next block
-            if (exitDirection == Direction.Left )
-                {
-                //keep y value
-                nextSpawnPos.x -= 2;
-                }
-            else if (exitDirection == Direction.Right)
-            {
-                //keep y value
-                nextSpawnPos.x += 2;
-            }
-            else if (exitDirection == Direction.Up)
-            {
-                //keep x value
-                nextSpawnPos.y += 2;
-            }
-            else if (exitDirection == Direction.Down)
-            {
-                //keep x value
-                nextSpawnPos.y -= 2;
-            }
+            // Setze die Position des Spielers auf die Position des spawnBlock
+            var nextSpawnPos = spawnBlock.transform.position;
+
             gameManager.SetPlayerSpawnPositionForNextBlock(nextSpawnPos);
             gameManager.SetCurrentBlock(nextBlockID);
       
